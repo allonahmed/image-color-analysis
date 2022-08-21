ef96cabc7a774a8111f1e0d61c8f8d805eaa321f
allonahmed <allonahmed@mec.science>
2022-08-21 15:14:35 -0400

    connected mysql db to app

--- sneaker-db/server.js
+++ sneaker-db/server.js
@@ -12,6 +12,27 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
+var db = mysql.createConnection({
+  // setup our mysql db connection
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "sneaker-db",
+  port: "3306",
+});
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "sneaker-db",
+  port: "3306",
+});
+pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
 app.use(cors(corsOptions));
 
 const PORT = 2020;
