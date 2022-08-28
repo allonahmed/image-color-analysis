62c26660db5090175659835ee293dcb848c1838f
allonahmed <allonahmed@mec.science>
2022-08-28 02:48:41 -0400

    gradient for input borderns

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { DisplayColors } from '../features/imageRender/DispayColors';
 import { Upload } from '../features/ImageUpload/Upload';
+import { SneakerSelect } from '../features/sneakerRender/SneakerSelect';
+
+import '../styles/home.css';
 
 function Home() {
   return (
-    <div>
-      <Upload />
+    <div className='home-container'>
+      <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <Upload />
+        <p style={{padding: '0 20px', fontSize: '20px'}}>or</p>
+        <SneakerSelect />
+      </div>
       <DisplayColors />
     </div>
   );
