007981b9b3be1eefddd0d522c6f8ba807cec2289
allonahmed <allonahmed@mec.science>
2022-08-29 18:55:07 -0400

    added testing function for adjusting background images

--- server/utils/colors.py
+++ server/utils/colors.py
@@ -1,5 +1,26 @@
 from colorthief import ColorThief
 from PIL import Image
+import numpy as np
+
+def remove_background():
+    im = Image.open('./assets/fileName.jpg')
+    im = im.convert('RGBA')
+    data = np.array(im)
+    # just use the rgb values for comparison
+    rgb = data[:,:,:3]
+    color = [0,0,0]   # Original value
+    black = [0,0,0, 255]
+    white = [255,255,0,255]
+    mask = np.all(rgb == color, axis = -1)
+    # change all pixels that match color to white
+    data[mask] = white
+
+    # change all pixels that don't match color to black
+    ##data[np.logical_not(mask)] = black
+    new_im = Image.fromarray(data)
+    new_im.save('./assets/redbg.png', "PNG")
+
+remove_background()
 
 #using color theif module to generate rgb of most used color in img returns array
 def ct_dominant_color(image_path):
