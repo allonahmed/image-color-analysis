07ead4f04b1bc798f5acec67240f069042ecf550
allonahmed <allonahmed@mec.science>
2022-09-05 13:01:32 -0400

    working version of eye dropper

--- client/src/components/EyeDropper.tsx
+++ client/src/components/EyeDropper.tsx
@@ -3,9 +3,9 @@ import { EyeDropper as ColorDropper } from 'react-eyedrop';
 
 
 export const EyeDropper = () => {
-  const [color, setColor] = useState<any>(null);
+  const [color, setColor] = useState<any>('#bada55');
 
-  const getColor = ({rgb}: any) => {
+  const getColor = ({rgb, hex}:any) => {
     setColor(rgb);
   }; 
 
@@ -13,12 +13,14 @@ export const EyeDropper = () => {
 
   return (
     <div>
+      <div style={{background: color, height: '50px', width: '50px'}}>
+        djdj
+      </div>
       <ColorDropper
         onChange={getColor}
       >
-        select color
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3ItjUjmdyS3oifHWUhSGsSpNphIZ38hZ3Obdz2FjU&s'/>
       </ColorDropper>
-      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3ItjUjmdyS3oifHWUhSGsSpNphIZ38hZ3Obdz2FjU&s'/>
     </div>
   );
 };
