2b0b9ef66d9baaedc41df165ff5a0b1d8dff1ccf
allonahmed <allonahmed@mec.science>
2022-09-06 20:43:03 -0400

    img uploaded converted to canvas

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -1,26 +1,25 @@
 import React, {useEffect} from 'react';
+import { useAppSelector } from '../hooks';
 
 export const Canvas : React.FunctionComponent = () => {
 
+  const image = useAppSelector((state) => state.image.image);
+  console.log(image);
   useEffect(()=> {
-    window.onload = () =>{
-      const img : any = document.getElementById('img');
+    if (image){
+      const img : any = new Image();
+      img.src = image;
       const canvasElement : any  = document.getElementById('canvas');
       const ctx = canvasElement.getContext('2d');
       const canvas = ctx.canvas;
       canvas.height = img.height;
       canvas.width = img.width;
       ctx.drawImage(img, 0,0, img.width, img.height);
-    };
-  });
+    }
+  },[image]);
 
   return (
     <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-      <img 
-        id='img' 
-        src= 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F13%2F2015%2F04%2F05%2Ffeatured.jpg&q=60' 
-        style={{height: '400px'}}
-      />
       <canvas id='canvas' style={{border: '1px solid black' }}></canvas>
     </div>
   );
