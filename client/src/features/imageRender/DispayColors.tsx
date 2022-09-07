f18f3a9815ec0b0b615b21b3c9353802fbe6838f
allonahmed <allonahmed@mec.science>
2022-09-06 23:31:49 -0400

    convert rgb to hex added to helpers

--- client/src/features/imageRender/DispayColors.tsx
+++ client/src/features/imageRender/DispayColors.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { useAppSelector } from '../../hooks';
 import { imageColors } from '../../redux/reducers/image';
 import { Canvas } from '../../components/Canvas';
-
+import { rgbToHex } from '../../helpers';
 import '../../styles/Imagedisplay.css';
 
 const toPercent = (number: number) => {
   return `${(number.toFixed(2))}%`;
 };
+
+
 
 export const DisplayColors: React.FC = () => {
   const { image, imageColors, current} = useAppSelector(state => state.image);
@@ -37,17 +39,19 @@ export const DisplayColors: React.FC = () => {
               {imageColors.map((color: any, id: number) => {
                 return (
                   <div key={id} className='palette-item'>
-                    <div className='palette-rgb'>
-                      {`rgb(${color[0]}, ${color[1]}, ${color[2]})`}
-                    </div>
                     {/* <div style={{fontSize:'8px'}}>rgb({color.color[0]},{color.color[1]}, {color.color[2]})</div> */}
                     <div
                       className='palette-color'
                       style={{
                         // backgroundColor: `rgb(${color.color[0]}, ${color.color[1]}, ${color.color[2]}`,
                         backgroundColor:`rgb(${color[0]}, ${color[1]}, ${color[2]})`,
+                        boxShadow: `0 0 5px 1px rgb(${color[0]}, ${color[1]}, ${color[2]})`
                       }}
-                    />
+                    >
+                      <div className='palette-hex'>
+                        {rgbToHex(color[0], color[1], color[2])}
+                      </div>
+                    </div>
                   </div>
                 );
               })}
