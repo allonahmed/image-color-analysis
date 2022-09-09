2cba7bb8a264e3046ee2ec3b8a12c3fdf1d971f9
allonahmed <allonahmed@mec.science>
2022-09-09 15:08:46 -0400

    added hextorgb conversion

--- client/src/helpers/index.ts
+++ client/src/helpers/index.ts
@@ -1,5 +1,16 @@
-const rgbToHex = (r: any, g : any, b: any) => {
+// converts rgb string to hex format
+const rgbToHex = (r: number, g : number, b: number) => {
   return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 };
 
-export {rgbToHex};
+//converts hex to rgb
+const hexToRgb = (hex: string) =>{
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+};
+
+export { rgbToHex, hexToRgb };
