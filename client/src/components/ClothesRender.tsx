d3d154a2b86e5901b569ce2e40dd08636ca35070
allonahmed <allonahmed@mec.science>
2022-10-03 08:34:55 -0400

    added styling

--- client/src/components/ClothesRender.tsx
+++ client/src/components/ClothesRender.tsx
@@ -9,17 +9,23 @@ type Props = {
 }
 
 const Clothes : React.FunctionComponent<{ item: any }> = ({ item }) => {
-  console.log(item.img_url);
   return (
-    <div> 
-      <img src={item.img_url} style={{width: '200px', height: '200px'}}/>
+    <div style={{display:'flex', flexDirection:'column'}}> 
+      <img src={item.img_url} style={{width: '320px', height: '350px'}}/>
+      <div style={{width: '320px'}}>{item.name}</div>
     </div>  
   );
 };
 
 export const ClothesRender : React.FunctionComponent<Props> = ({ clothesData }) => {
   return (
-    <div>
+    <div style={{
+      display:'grid', 
+      gridTemplateColumns: 'repeat(3, 1fr)', 
+      gridColumnGap: '10px', 
+      gridRowGap: '10px',
+      justifyItems:'center',
+    }}>
       {
         clothesData.map((item: any, index: number)=> {
           return (
